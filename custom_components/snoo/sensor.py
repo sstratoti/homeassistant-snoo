0b1386e902f8c40482f3d0857a9d7bbe1761af51
Peter Hardy <peter@dropbear.id.au>
2020-02-12 01:23:54 +1100

    Support device attributes.

--- custom_components/snoo/sensor.py
+++ custom_components/snoo/sensor.py
@@ -23,7 +23,8 @@ class SnooStateSensor(Entity):
     """Representation of the Snoo status."""
 
     def __init__(self):
-        self._state = SnooState.NONE
+        self._setup_state()
+        self._setup_attributes()
         self._client = Client()
 
     @property
@@ -36,15 +37,56 @@ class SnooStateSensor(Entity):
         """Return the state of the sensor."""
         return self._state.value
 
+    @property
+    def device_state_attributes(self):
+        """Return device specific state attributes."""
+        return self._attributes
+
+    def _setup_state(self):
+        self._state = SnooState.NONE
+
+    def _setup_attributes(self):
+        # level:
+        #   When awake or asleep, this is 0. When soothing,
+        #   this is the soothing level between 1 and 4.
+        # sleep_start_time:
+        #   When awake, this is "". When asleep or soothing, this is the
+        #   timestamp of when this sleep session started. That is, when
+        #   we transitioned from awake to asleep. Transitions between
+        #   soothing levels are ignored.
+        self._attributes = {
+            "level": 0,
+            "sleep_start_time": ""
+        }
+
     def update(self):
         """Fetch new state data for the sensor."""
         session = self._client.get_current_session()
         if session["end_time"]:
+            self._attributes["level"] = 0
+            self._attributes["sleep_start_time"] = ""
             self._state = SnooState.AWAKE
         elif session["level"] in ["BASELINE", "WEANING_BASELINE"]:
+            # TODO: What should we doo about SnooState.NONE here?
+            if self._state is SnooState.AWAKE:
+                self._attributes["sleep_start_time"] = session["start_time"]
+            self._attributes["level"] = 0
             self._state = SnooState.ASLEEP
-        else:
+        elif session["level"] == "LEVEL1":
+            self._attributes["level"] = 1
             self._state = SnooState.SOOTHING
+        elif session["level"] == "LEVEL2":
+            self._attributes["level"] = 2
+            self._state = SnooState.SOOTHING
+        elif session["level"] == "LEVEL3":
+            self._attributes["level"] = 3
+            self._state = SnooState.SOOTHING
+        elif session["level"] == "LEVEL4":
+            self._attributes["level"] = 4
+            self._state = SnooState.SOOTHING
+        else:
+            # TODO: Raise an exception here?
+            self._state = SnooState.NONE
 
 
 class SnooState(Enum):
